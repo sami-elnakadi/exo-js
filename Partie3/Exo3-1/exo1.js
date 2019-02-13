43dcb1471388e97f3a68228f373a9d43f0a34b49
user <sami.elnakadi@gmail.com>
2019-02-13 11:10:21 +0100

    partie3

---
+++ Partie3/Exo3-1/exo1.js
@@ -0,0 +1,7 @@
+
+ function test(){
+    document.getElementById("image1").src = "/home/user/Bureau/Js/Exo JS/exo-js/Partie3/Exo3-1/img/image1_2.jpg";
+ };
+ function test2(){
+    document.getElementById("image1").src = "/home/user/Bureau/Js/Exo JS/exo-js/Partie3/Exo3-1/img/image1.jpg";
+ };
---
+++ Partie3/challengetest/challengetest.js
@@ -0,0 +1,1 @@
+let a = ['Coucou','Ca va','Ca peut aller'];
