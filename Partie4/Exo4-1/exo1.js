b18cb60f83db2965ba304ce9ff83ecfc745c04e4
user <sami.elnakadi@gmail.com>
2019-02-14 10:14:20 +0100

    partie4

---
+++ Partie4/Exo4-1/exo1.js
@@ -0,0 +1,8 @@
+function bord(){
+    let i = document.getElementById('image1');
+    i.style.border = "3px solid red";
+}
+function bordno(){
+    let i = document.getElementById('image1');
+    i.style.border = "none";
+}
