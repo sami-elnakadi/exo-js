12a6c8d88f44e7e36f0ac308fb697caf755538d4
user <sami.elnakadi@gmail.com>
2019-02-19 15:26:12 +0100

    fin pendu?

--- Pendu/pendu.js
+++ Pendu/pendu.js
@@ -1,20 +1,52 @@
 let answer = ['b','o','n','j','o','u','r'];
+//['m','a','c','h','i','n','e']
 let guess = ['','','','','','',''];
-let quest = window.prompt('Entrez une lettre s.v.p: ');
-let rep = answer.includes(quest);
-
-
+      
 function guessLetter (){
- if(rep === true) {
-    guess[quest, indexOf(answer)]=quest;
-        quest;
- }
- else if (rep===false){
-    quest;
- }    
-    
+   let quest = prompt('Entrez une lettre s.v.p: ');
+// Fonctionne, mais pas pour les doubles lettres:   
+//    let pos;
+//    for(let i=0; i<guess.length;i++){
+//       for(let j=0; j<guess.length;j++){
+//         answer[i]=guess[j];
+//     }
+     
 
-}console.log(guess);
-console.log(rep);
-console.log(answer);
-console.log(quest);
+//    if(answer.includes(quest)) {
+ 
+//       if(guess.includes(quest) === false){
+//          pos = answer.indexOf(quest);
+//          guess.splice(pos,1,quest);
+//          alert('Bien joué!')
+//       }
+//       if(guess.value===answer.value){
+//          alert('GG à toi, tu as trouvé le mot!');
+//       }
+//    }
+//    else 
+//       alert('Nop, essaies encore!');
+
+// }
+
+for(let i=0; i<guess.length;i++){
+   if(quest === answer[i]){
+      guess[i] = quest;
+      alert('Bravo, vous avez trouvé '+guess[i]);
+      alert('Vous avez '+guess);
+      }
+   }
+if(answer.includes(quest)===false) {   
+   alert('Nop, essaies encore!');
+   }
+
+let over = 0;
+for(let i=0; i<guess.length;i++){
+   if(guess[i] === answer[i]){
+      over++;
+      }
+   if(over===answer.length){
+      alert('Bien joué, tu as trouvé le mot!')
+      }
+
+   }
+}
